b9ef119bdc99f1afb412996db8eeb3aaaf9dc401
equan <equan06@gmail.com>
2020-01-09 09:37:56 -0800

    allow threading of requests

---
+++ londontest.py
@@ -0,0 +1,181 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+import os
+import sqlite3
+from multiprocessing.dummy import Pool as ThreadPool
+from datetime import datetime
+
+
+
+def sql_connect(dbname):
+    """
+    Input: dbname: name of db file (dbname.db) in local dir
+    Ouput: sqlite3 connection object
+    """
+    dirname = os.path.dirname(__file__)
+    dbpath = os.path.join(dirname, dbname)
+    print('Connecting to: ' + dbpath)
+    try:
+        con = sqlite3.connect(dbpath)
+    except:
+        print('SQL connection error')
+        raise
+    return con
+
+def sql_create_table(con, table_name):
+    """
+    Input: con: SQL cursor object, table_name: String
+    Output: SQL cursor object
+    """
+    cur = con.cursor()
+    sql = f"""
+    CREATE TABLE {table_name} (
+        country TEXT,
+        club TEXT,
+        category TEXT,
+        id INTEGER PRIMARY KEY,
+        place_gender INTEGER,
+        place_category INTEGER,
+        place_overall INTEGER,
+        split_5K TEXT,
+        split_5K_diff TEXT,
+        split_10K TEXT,
+        split_10K_diff TEXT,
+        split_15K TEXT,
+        split_15K_diff TEXT,
+        split_20K TEXT,
+        split_20K_diff TEXT,
+        split_Half TEXT NOT NULL,
+        split_Half_diff TEXT,
+        split_25K TEXT,
+        split_25K_diff TEXT,
+        split_30K TEXT,
+        split_30K_diff TEXT,
+        split_35K TEXT,
+        split_35K_diff TEXT,
+        split_40K TEXT,
+        split_40K_diff TEXT,
+        split_Finish TEXT NOT NULL,
+        split_Finish_diff TEXT,
+        gender TEXT
+    )
+    """
+    try:
+        cur.execute(sql)
+        print(f'Table: {table_name} created')
+    except:
+        print('Table created already')
+    return cur
+
+def sql_insert(con, table_name, ath):
+    """
+    Input: cur: SQL connection object, table_name: String, ath: list containing individual athlete values
+    Output: SQL cursor object
+    """
+    cur = con.cursor()
+    sql = f"""
+    INSERT INTO {table_name}
+    (country, club, category, id, place_gender, place_category, place_overall, gender,
+    split_5K, split_5K_diff, split_10K, split_10K_diff, split_15K, split_15K_diff,
+    split_20K, split_20K_diff, split_Half, split_Half_diff, split_25K, split_25K_diff,
+    split_30K, split_30K_diff, split_35K, split_35K_diff, split_40K, split_40K_diff,
+    split_Finish, split_Finish_diff) VALUES
+    (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    """
+    try:
+        cur.execute(sql, ath)
+    except:
+        print('SQL Insert failure at ')
+        print(ath)
+        raise
+    return cur
+
+def get_ath_data(ath_soup):
+    """
+    Input: ath_soup: BeautifulSoup object of an athlete's results page
+    Ouput: dict containing all 28 athlete attributes
+    """
+    ath = []
+    # omit names from data, index athetes by bib/runner number
+
+    vars = ['Name', 'Club', 'Category', 'Runner Number', 'Place (Gender)', 'Place (Category)', 'Place (Overall)']
+    soup = ath_soup.find_all('th', text=vars)
+    name_found = False
+    flag = False
+    for s in soup:
+        if s.text == 'Name':
+            if name_found:
+                continue
+            else:
+                name = s.find_next('td').string
+                try:
+                    club = re.search(r'\(([A-Z]+)\)', name).group(1)
+                    name_found = True
+                except:
+                    flag = True
+                    club = 'None'
+                ath.append(club)
+        else:
+            res = s.find_next('td').string
+            ath.append(res)
+    ath.append('M')
+
+
+
+    vars = ['5K', '10K', '15K', '20K', 'Half', '25K', '30K', '35K', '40K', 'Finish']
+    soup = ath_soup.find_all('th', text=vars)
+    for s in soup:
+        res = s.find_next_siblings('td')
+        ath.append(res[1].string)
+        ath.append(res[2].string)
+    if flag:
+        print(ath)
+    return ath
+
+
+
+
+def page_opener(ath_page):
+    ath_url = 'https://results.virginmoneylondonmarathon.com/2019/' + ath_page.get('href')
+    ath_r = requests.get(ath_url)
+    ath_soup = BeautifulSoup(ath_r.content, "html.parser")
+    ath = get_ath_data(ath_soup)
+    return ath
+
+if __name__ == '__main__':
+    table_name = 'test'
+    con = sql_connect(f'{table_name}.db')
+
+    # run once initially
+    sql_create_table(con, table_name)
+
+    urlleft = 'https://results.virginmoneylondonmarathon.com/2019/?page='
+    urlright = '&event=MAS&num_results=1000&pid=list&search%5Bgender%5D=M&search%5Bage_class%5D=%25'
+
+    try:
+        for page in range(17, 27):
+            curr_time = datetime.now()
+            page_r = requests.get(urlleft + str(page) + urlright)
+            soup = BeautifulSoup(page_r.content, "html.parser")
+            ath_pages = soup.select('h4 > a')
+            pool = ThreadPool(4)
+            res = pool.map(page_opener, ath_pages)
+            pool.close()
+            pool.join()
+            for ath in res:
+                sql_insert(con, table_name, ath)
+
+            # commit changes every page = 1000 rows
+            con.commit()
+            new_time = datetime.now()
+            elapsed_time = (new_time - curr_time).total_seconds() / 60
+            print(f'Commit successful')
+            print(f'Table: {table_name} | Page: {page} complete |  Time: {elapsed_time} min')
+
+    except:
+        print(f'Failed on Page: {page}')
+        con.rollback()
+        print('Rollback to last commit successful.')
+        con.close()
+        raise
